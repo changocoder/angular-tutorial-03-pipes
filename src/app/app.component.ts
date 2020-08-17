db53ed4beb1d4a0d12e3b014bd42a992b1d6f82e
hugo <hugogher@gmail.com>
2020-08-17 10:25:37 -0300

    se agrega configuracion local para soporte spanish

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   nombre  = 'Capitan America';
-  listaNumeros = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ];
-  PI: number = Math.PI;
-  porcentaje = 0.234;
+  listaNumeros  = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ];
+  PI: number    = Math.PI;
+  porcentaje    = 0.234;
+  salario       = 123.4;
+  fecha: Date   = new Date();
+
+  valorPromesa  = new Promise<string>( (resolve) => {
+    setTimeout(() => {
+      resolve('Habemus Data');
+    }, 3600);
+
+  });
+
+  heroe = {
+      nombre: 'Logan',
+      clave: 'Wolverine',
+      edad: 500,
+      direccion: {
+          calle: 'Proyectada 5',
+          casa: 295
+      }
+
+  };
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
+
+import { registerLocaleData } from '@angular/common';
+
+ // importar locales
+import localePy from '@angular/common/locales/es-PY';
+import localePt from '@angular/common/locales/pt';
+import localeEn from '@angular/common/locales/en';
+import localeEsAr from '@angular/common/locales/es-AR';
+
+ // registrar los locales con el nombre que quieras utilizar a la hora de proveer
+registerLocaleData(localePy, 'es');
+registerLocaleData(localePt, 'pt');
+registerLocaleData(localeEn, 'en');
+registerLocaleData(localeEsAr, 'es-Ar');
+
+
 
 import { AppComponent } from './app.component';
 
@@ -10,7 +26,12 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: LOCALE_ID,
+      useValue: 'es-Ar'
+  }
+],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
